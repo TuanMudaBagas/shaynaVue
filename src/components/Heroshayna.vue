1e86e04318f2edbbb419245c2281c3692bc57966
TuanMudaBagas <bagasfd24@gmail.com>
2020-02-20 17:37:04 +0700

    ok

---
+++ src/components/Heroshayna.vue
@@ -0,0 +1,53 @@
+<template>
+	<!-- Hero Section Begin -->
+	<section class="hero-section">
+		<Carosel class="hero-items" :items="1" :nav="false" :autoplay="true ">
+			<div class="single-hero-items set-bg" style="background-image: url('img/hero-1.jpg')">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-5">
+							<span>Bag,kids</span>
+							<h1>Black friday</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur
+								adipisicing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore
+							</p>
+							<a href="#" class="primary-btn">Shop Now</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="single-hero-items set-bg" style="background-image: url('img/hero-2.jpg')">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-5">
+							<span>Bag,kids</span>
+							<h1>Black friday</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur
+								adipisicing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore
+							</p>
+							<a href="#" class="primary-btn">Shop Now</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Carosel>
+	</section>
+	<!-- Hero Section End -->
+</template>
+
+<script>
+// Import
+import Carosel from "vue-owl-carousel";
+
+export default {
+	name: "Hero",
+	components: {
+		Carosel
+	}
+};
+</script>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="home">
+		<Header />
+		<Hero />
+		<Item />
+		<Inta />
+		<Brand />
+		<Footer />
+	</div>
+</template>
+
+<script>
+import Header from "./../components/Headershayna";
+import Hero from "./../components/Heroshayna";
+import Item from "./../components/Itemshayna";
+import Inta from "./../components/Instashayna";
+import Brand from "./../components/Brandshayna";
+import Footer from "./../components/Footershayna";
+export default {
+	name: "Home",
+	components: {
+		Header,
+		Hero,
+		Item,
+		Inta,
+		Brand,
+		Footer
+	}
+};
+</script>
